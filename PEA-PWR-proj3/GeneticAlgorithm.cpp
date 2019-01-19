a43f5852f3fd990ac443e961f2db7e171395d066
Kajetan <kajetan123@gmail.com>
2019-01-19 15:37:14 +0100

    Grading & init selection

--- PEA-PWR-proj3/GeneticAlgorithm.cpp
+++ PEA-PWR-proj3/GeneticAlgorithm.cpp
@@ -3,6 +3,40 @@
 #include <vector>
 #include <random>
 
+
+// *********************************************************************************************
+// Initializes shortestRoute and shortestRouteValue using greedy aproach treating startNode as 0
+// *********************************************************************************************
+void GeneticAlgorithm::initRoute()
+{
+	if (shortestRoute != nullptr) {
+		delete shortestRoute;
+	}
+	shortestRoute = new int[cities.getNodesNumber()];
+	shortestRoute[0] = startingNode;
+
+	bool* alreadyVisited = new bool[cities.getNodesNumber()];
+	for (int i = 0; i < cities.getNodesNumber(); i++) {
+		alreadyVisited[i] = false;
+	}
+	alreadyVisited[startingNode] = true;
+
+	for (int i = 0; i < cities.getNodesNumber() - 1; i++) {
+		int min = INT_MAX;
+		int chosen = -1;
+		for (int j = 0; j < cities.getNodesNumber(); j++) {
+			if (cities.getElement(shortestRoute[i], j) < min && !alreadyVisited[j]) {
+				min = cities.getElement(shortestRoute[i], j);
+				chosen = j;
+			}
+		}
+		alreadyVisited[chosen] = true;
+		shortestRoute[i + 1] = chosen;
+	}
+
+	shortestRouteValue = calculateRouteLength(shortestRoute);
+}
+
 // *********************************************************************************************
 // Calculates length of a given route
 // *********************************************************************************************
@@ -32,6 +66,51 @@ void GeneticAlgorithm::setRandomRoute(int * route)
 		route[i] = possibleCities[randomNum];
 		possibleCities.erase(possibleCities.begin() + randomNum);
 	}
+}
+
+// *********************************************************************************************
+// Returns grade of a given route -> the higher the better
+// *********************************************************************************************
+int GeneticAlgorithm::gradeRoute(int * route)
+{
+	return shortestRouteValue - calculateRouteLength(route);
+}
+
+// *********************************************************************************************
+// Normalize grades to be greater than zero
+// *********************************************************************************************
+void GeneticAlgorithm::normalizeGrades(int * grades, int populationSize) {
+	int min = 0;
+	for (int i = 0; i < populationSize; i++) {
+		if (grades[i] < min) {
+			min = grades[i];
+		}
+	}
+	min *= -1;
+	min++;
+	for (int i = 0; i < populationSize; i++) {
+		grades[i] += min;
+	}
+}
+
+// *********************************************************************************************
+// Copies one population into another
+// *********************************************************************************************
+void GeneticAlgorithm::copyPopulation(int ** from, int ** to, int populationSize)
+{
+	for (int j = 0; j < populationSize; j++) {
+		for (int i = 0; i < cities.getNodesNumber(); i++) {
+			to[j][i] = from[j][i];
+		}
+	}
+}
+
+// *********************************************************************************************
+// Selects parents into selection based on roullete method using grades
+// *********************************************************************************************
+void GeneticAlgorithm::selectByRoullete(int * grades, int * selection)
+{
+
 }
 
 // *********************************************************************************************
@@ -67,27 +146,77 @@ string GeneticAlgorithm::printSource()
 
 // *********************************************************************************************
 // Where the Magic takes place, solves TSP using genetic algorithm
+// Selection modes: 1 - Grades proportionate selection - IN DEVELOPMENT
+//					2 - Tournament selection - TO DO
 // *********************************************************************************************
-void GeneticAlgorithm::solve(const int populationSize)
+void GeneticAlgorithm::solve(const int populationSize, const int iterations, const int selectionMode)
 {
 	if (cities.isEmpty()) {
 		return;
 	}
+	// Init shortestroute/shortestroutevalue using greedy aproach
+	initRoute();
 
 	// Init population
+	int** population;
 	population = new int*[populationSize];
 	for (int i = 0; i < populationSize; i++) {
 		population[i] = new int[cities.getNodesNumber()];
 		setRandomRoute(population[i]);
 	}
+	int** newPopulation;
+	newPopulation = new int*[populationSize];
+	for (int i = 0; i < populationSize; i++) {
+		newPopulation[i] = new int[cities.getNodesNumber()];
+	}
+	copyPopulation(population, newPopulation, populationSize);
+	int * selectedParents = new int[populationSize];
+
+	// Init grades
+	int* grades = new int[populationSize];
+	for (int j = 0; j < populationSize; j++) {
+		grades[j] = gradeRoute(population[j]);
+	}
+	normalizeGrades(grades, populationSize);
+
+	// **********
+	// Main magic
+	// **********
+	for (int i = 0; i < iterations; i++) {
+		// Selecting parents pairs
+		switch (selectionMode) {
+		case 1:
+			selectByRoullete(grades, selectedParents);
+			break;
+		case 2:
+			break;
+		}
+		// Crossing / Mutating
+		
+		
+		
+		// Grading new population
+		for (int j = 0; j < populationSize; j++) {
+			grades[j] = gradeRoute(newPopulation[j]);
+		}
+		normalizeGrades(grades, populationSize);
+		for (int j = 0; j < populationSize; j++) { // test
+			cout << to_string(grades[j]) << ' ';
+		}
+		cout << endl; // test
+	}
 
 
-
-
+	// Cleanup grades
+	delete[] grades;
 	// Cleanup population
 	for (int i = 0; i < populationSize; i++) {
 		delete[] population[i];
 	}
 	delete[] population;
-	population = nullptr;
+	for (int i = 0; i < populationSize; i++) {
+		delete[] newPopulation[i];
+	}
+	delete[] newPopulation;
+	delete[] selectedParents;
 }
--- PEA-PWR-proj3/GeneticAlgorithm.h
+++ PEA-PWR-proj3/GeneticAlgorithm.h
@@ -8,12 +8,16 @@ private:
 	AdjacencyMatrix cities;
 	int* shortestRoute = nullptr;
 	int shortestRouteValue = INT_MAX;
-	int** population = nullptr;
 	int startingNode = 0;
 	mt19937 rng;
 
+	void initRoute();
 	int calculateRouteLength(int * route);
 	void setRandomRoute(int * route);
+	int gradeRoute(int * route);
+	void normalizeGrades(int * grades, int populationSize);
+	void copyPopulation(int** from, int** to, int populationSize);
+	void selectByRoullete(int * grades, int * selection);
 
 public:
 	GeneticAlgorithm();
@@ -22,6 +26,6 @@ public:
 	int setSource(string filename);
 	string printSource();
 
-	void solve(int populationSize);
+	void solve(const int populationSize, const int iterations, const int selectionMode = 1);
 };
 
